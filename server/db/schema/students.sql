db36e17fe3486188ea242dc2d0ff46a4909d8535
Bowen Wang <bowensw@uci.edu>
2024-11-22 18:17:40 -0800

    finished

---
+++ server/db/schema/students.sql
@@ -0,0 +1,7 @@
+CREATE TYPE LEVEL AS ENUM('beginner','intermediate','advanced');
+
+
+CREATE TABLE Student (  
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    level LEVEL NOT NULL
+  );
