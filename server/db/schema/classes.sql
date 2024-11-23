e4a6f5444d595af0b0b499fff52b8801df560afb
Alex Espejo <aespejo102@gmail.com>
2024-11-22 18:13:03 -0800

    create enum

--- server/db/schema/classes.sql
+++ server/db/schema/classes.sql
@@ -1,3 +1,5 @@
+CREATE TYPE LEVEL AS ENUM ("beginner", "intermediate", "advanced")
+
 CREATE TABLE IF NOT EXISTS public.classes
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
@@ -5,9 +7,8 @@ CREATE TABLE IF NOT EXISTS public.classes
     description TEXT,
     location VARCHAR(256) NOT NULL,
     capacity INT NOT NULL,
-    level character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    level LEVEL, 
     costume TEXT NOT NULL,
     CONSTRAINT class_pkey PRIMARY KEY (id),
-    CONSTRAINT level_check CHECK (level::text = ANY (ARRAY['beginner'::character varying, 'intermediate'::character varying, 'advanced'::character varying]::text[]))
 )
 
