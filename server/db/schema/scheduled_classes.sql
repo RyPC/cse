bc868b8ca555a1df66758265704e3d0aaf322a45
Joshua Micah Sullivan <joshee.sullivan@gmail.com>
2024-11-23 22:01:33 -0800

    josh - Fixed primary and foreign key constraints

--- server/db/schema/scheduled_classes.sql
+++ server/db/schema/scheduled_classes.sql
@@ -1,9 +1,11 @@
 DROP TABLE IF EXISTS scheduled_classes CASCADE;
 
 CREATE TABLE scheduled_classes (
-  class_id INTEGER PRIMARY KEY NOT NULL,
-  date DATE PRIMARY KEY NOT NULL,
+  class_id INTEGER NOT NULL,
+  date DATE NOT NULL,
   start_time TIME NOT NULL,
-  end_time TIME NOT NULL
+  end_time TIME NOT NULL,
+  PRIMARY KEY (class_id, date),
+  FOREIGN KEY (class_id) REFERENCES classes(id)
 );
 
