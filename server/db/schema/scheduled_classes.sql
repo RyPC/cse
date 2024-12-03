d5487ca0ca6c3318b0f0d9c2f7dd9b693ee00e58
Aditya Agrawal <58559719+aagrawal05@users.noreply.github.com>
2024-12-02 22:50:04 -0800

    Update scheduled_classes.sql
    Added cascade delete to foreign key

--- server/db/schema/scheduled_classes.sql
+++ server/db/schema/scheduled_classes.sql
@@ -6,6 +6,5 @@ CREATE TABLE scheduled_classes (
   start_time TIME NOT NULL,
   end_time TIME NOT NULL,
   PRIMARY KEY (class_id, date),
-  FOREIGN KEY (class_id) REFERENCES classes(id)
+  FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE CASCADE
 );
-
