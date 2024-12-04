cc52868ad8447f6ee5faca8c1d0033daa7cf26d6
Boyuan Liu <boyuanliu6@yahoo.com>
2024-12-04 15:03:02 -0800

    modified the order of CONSTRAINTs and CONSTRAINT names for Pk

--- server/db/schema/class_enrollments.sql
+++ server/db/schema/class_enrollments.sql
@@ -4,13 +4,13 @@ CREATE TABLE IF NOT EXISTS class_enrollments (
     class_id INTEGER NOT NULL,
     attendance DATE NOT NULL,
 
+    CONSTRAINT Pk_class_enrollments
+        PRIMARY KEY(student_id, class_id, id),
+
     CONSTRAINT fk_student
         FOREIGN KEY(student_id)
         REFERENCES student(id)
         ON DELETE CASCADE,
-
-    CONSTRAINT Pk_student_class_id
-        PRIMARY KEY(student_id, class_id, id),
     
     CONSTRAINT fk_class
         FOREIGN KEY(class_id)
