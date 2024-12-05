3957e521cb5c86754e2e3cecdc429e356bac24f8
Bowen Wang <bowensw@uci.edu>
2024-12-05 01:08:07 -0800

    added cascade

--- server/db/schema/students.sql
+++ server/db/schema/students.sql
@@ -1,5 +1,8 @@
+CREATE TYPE LEVEL AS ENUM('beginner','intermediate','advanced');
+
+
 CREATE TABLE Student (
     id INTEGER NOT NULL PRIMARY KEY,
-    level ENUM('beginner', 'intermediate', 'advanced') NOT NULL,
-    CONSTRAINT fk_user_student FOREIGN KEY (id) REFERENCES User (id)
+    level LEVEL NOT NULL,
+    CONSTRAINT fk_user_student FOREIGN KEY (id) REFERENCES User (id) ON DELETE CASCADE
 );
--- server/db/schema/teachers.sql
+++ server/db/schema/teachers.sql
@@ -2,5 +2,5 @@ CREATE TABLE Teachers (
     id INTEGER NOT NULL PRIMARY KEY,
     experience TEXT,
     is_activated BOOL NOT NULL,
-    CONSTRAINT fk_user_teacher FOREIGN KEY (id) REFERENCES User (id)
+    CONSTRAINT fk_user_teacher FOREIGN KEY (id) REFERENCES User (id) ON DELETE CASCADE
  );
