942f2d5a02bec172df1cdebed58a444524183965
Brandon <brandonpeng06@gmail.com>
2024-12-04 20:06:18 -0800

    Added cascade on delete to foreign keys

--- server/db/schema/event_enrollments.sql
+++ server/db/schema/event_enrollments.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS event_enrollments (
   student_id INTEGER NOT NULL,
   event_id INTEGER NOT NULL,
   attendance BOOLEAN NOT NULL,
-  FOREIGN KEY (student_id) REFERENCES students (id),
-  FOREIGN KEY (event_id) REFERENCES events (id)
+  FOREIGN KEY (student_id) REFERENCES students (id) ON DELETE CASCADE,
+  FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
 );
