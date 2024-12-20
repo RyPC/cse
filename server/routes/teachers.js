523b426dee9505926614fbeefe61bf88dc3fe8a1
elliott <48373238+Satchel05@users.noreply.github.com>
2024-12-19 17:05:40 -0800

    start working on POST route

---
+++ server/routes/teachers.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { keysToCamel } from '../common/utils';
+import { db } from '../db/db-mysql';
+
+const teachersRouter = express.Router();
+teachersRouter.use(express.json());
+
+teachersRouter.post("/teachers", async(req, res) => {
+    try {
+        const { first_name, last_name, role, user_role, email, experience } = req.params;
+
+        await db.query(
+        `
+            INSERT INTO teachers(id, experience, is_activated)
+            VALUES
+            ($1, $2, $3, $4, $5, $6);`,
+            [
+                first_name,
+                last_name,
+                role,
+                user_role,
+                email,
+                experience
+        ],
+        );
+        
+        res.status(200).json({
+            status: "Success",
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: "Failed",
+            msg: err.message,
+        });
+    }
+});
