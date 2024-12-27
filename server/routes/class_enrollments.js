6f079710ba53bd5acb2afdada3fc99f4b6dc6a04
Alex Espejo <aespejo102@gmail.com>
2024-12-27 13:23:42 -0800

    creataed enrollment routes

--- server/routes/class_enrollments.js
+++ server/routes/class_enrollments.js
@@ -1,33 +1,45 @@
 import express from "express";
+import { format } from "mysql";
 
 import { keysToCamel } from "../common/utils";
+import { db } from "../db/db-pgp"; // TODO: replace this db with
 
 const classEnrollmentsRouter = express.Router();
 classEnrollmentsRouter.use(express.json());
 
-classEnrollmentsRouter.get("/class-enrollments/:id", async (req, res) => {
+classEnrollmentsRouter.get("/:id", async (req, res) => {
   try {
-     const classById = await db.query(`SELECT * FROM class_enrollments WHERE ${id};`);
+    const classById = await db.query(
+      `SELECT * FROM class_enrollments WHERE ${id};`
+    );
     res.status(200).json(classById);
   } catch (err) {
     res.status(500).send(err.message);
   }
 });
 
-classEnrollmentsRouter.get("/class-enrollments", async (req, res) => {
+classEnrollmentsRouter.get("/", async (req, res) => {
   try {
-     const classById = await db.query(`SELECT * FROM class_enrollments;`);
-    res.status(200).json(classById);
+    const classById = await db.query(`SELECT * FROM class_enrollments;`);
+    res.status(200).send(classById);
   } catch (err) {
     res.status(500).send(err.message);
   }
 });
 
-classEnrollmentsRouter.post("/class-enrollments", async (req, res) => {
+classEnrollmentsRouter.post("/class_enrollments", async (req, res) => {
+  const { student_id, class_id, enrollment_date } = req.body;
   try {
-    const data = {};
+    const createStudent = await db.query(
+      "INSERT INTO student (id, level, date) VALUES ($1, $2, $3)"
+    );
 
-    res.status(200).json(keysToCamel(data));
+    const classById = await db.query(
+      `INSERT INTO class_enrollments (student_id, class_id, attendance) VALUES ($1, $2, $3)`,
+      [student_id, class_id, enrollment_date]
+    );
+
+    res.status(200).send({ message: "Enrollment created successfully." });
   } catch (err) {
     res.status(500).send(err.message);
   }
