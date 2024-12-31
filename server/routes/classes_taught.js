ec512fb98e6ec55ee8e7b8481bde711b28fe3e43
Ryan Su <ryan.parkcity@gmail.com>
2024-12-30 17:22:35 -0700

    Fixed bugs with classes_taught routes

--- server/routes/classes_taught.js
+++ server/routes/classes_taught.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { keysToCamel } from "../common/utils";
-import { db } from "../server/db";
+import { db } from "../db/db-pgp";
 
 const classesTaughtRouter = express.Router();
 classesTaughtRouter.use(express.json());
@@ -10,7 +10,7 @@ classesTaughtRouter.use(express.json());
 classesTaughtRouter.get("/", async (req, res) => {
   try {
     // Query database
-    const query = `SELECT * FROM classes-taught;`;
+    const query = `SELECT * FROM classes_taught;`;
 
     const allClassesTaught = await db.query(query);
     res.status(200).json(keysToCamel(allClassesTaught));
@@ -23,17 +23,17 @@ classesTaughtRouter.get("/", async (req, res) => {
 classesTaughtRouter.post("/", async (req, res) => {
   try {
     // Destructure req.body
-    const { teacher_id, class_id } = req.body;
+    const { teacherId, classId } = req.body;
 
     // Construct query with parameters
-    const query = `INSERT INTO classes-taught (teacher_id, class_id) VALUES ($1, $2);`;
-    const params = [teacher_id, class_id];
+    const query = `INSERT INTO classes_taught (teacher_id, class_id) VALUES ($1, $2);`;
+    const params = [teacherId, classId];
 
     await db.query(query, params);
 
     res.status(200).send({
-      teacher_id: teacher_id,
-      class_id: class_id,
+      teacher_id: teacherId,
+      class_id: classId,
     });
   } catch (err) {
     res.status(500).send(err.message);
