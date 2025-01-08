279aaaa7d864b0f018e0ddab6cc7d6236c24d48d
Alyssia Tan <aly.tan@outlook.com>
2025-01-08 15:51:25 -0800

    put now works with any combination of fields

--- server/routes/class_videos.js
+++ server/routes/class_videos.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { keysToCamel } from "../common/utils";
-import { db } from "../db/db-pgp"; // TODO: replace this db with
+import { db } from "../db/db-pgp";
 
 const classVideosRouter = express.Router();
 classVideosRouter.use(express.json());
@@ -28,46 +28,65 @@ classVideosRouter.get("/", async (req, res) => {
 });
 
 classVideosRouter.post("/", async (req, res) => {
-    try {
-      const { title, s3_url, description, media_url, class_id } = req.body;
-      
-      const postData = await db.query(
-        `INSERT INTO class_videos (title, s3_url, description, media_url, class_id)
-         VALUES ($1, $2, $3, $4, $5) RETURNING id, title, s3_url, description, media_url, class_id;`,
-        [title, s3_url, description, media_url, class_id]
+  try {
+    const { title, s3Url, description, mediaUrl, classId } = req.body;
+    
+    const postData = await db.query(
+      `INSERT INTO class_videos (title, s3_url, description, media_url, class_id)
+        VALUES ($1, $2, $3, $4, $5) RETURNING id, title, s3_url, description, media_url, class_id;`,
+      [title, s3Url, description, mediaUrl, classId]
+    );
+
+    res.status(200).json(keysToCamel(postData)); //is this supposed to be .send instead?
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+classVideosRouter.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, s3Url, description, mediaUrl, classId } = req.body;
+
+    const { count } = (
+      await db.query(`SELECT COUNT(*) FROM class_videos WHERE id = $1`, [id])
+    )[0];
+
+    if(parseInt(count) === 1) {
+      const updatedClassVideos = await db.query(
+        `UPDATE class_videos SET
+        ${title ? 'title = $(title), ' : ''}
+        ${s3Url ? 's3_url = $(s3Url), ' : ''}
+        ${description ? 'description = $(description), ' : ''}
+        ${mediaUrl ? 'media_url = $(mediaUrl), ' : ''}
+        ${classId ? 'class_id = $(classId), ' : ''}
+        id = '${id}'
+          WHERE id = '${id}'
+          RETURNING *;`,
+        {
+          title, s3Url, description, mediaUrl, classId
+        },
       );
-  
-      res.status(200).json(keysToCamel(postData));
-    } catch (err) {
-      res.status(500).send(err.message);
+      res.status(200).send(keysToCamel(updatedClassVideos)); 
+    } else {
+      res.status(400).send("Video with provided id not found");
     }
-  });
+  }
+  catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
-  classVideosRouter.put("/:id", async (req, res) => {
-    try {
-      const { title, s3_url, description, media_url, class_id } = req.body;
-      
-      const updatedClassVideos = await db.query(`UPDATE class_videos
-        SET title = $1, s3_url = $2, description = $3, media_url = $4, class_id = $5
-        RETURNING id, title, s3_url, description, media_url, class_id;`,
-      [title, s3_url, description, media_url, class_id]);
-  
-      res.status(200).json(keysToCamel(updatedClassVideos));
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  });
+classVideosRouter.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
 
-  classVideosRouter.delete('/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-
-      const deletedClassVideos = await db.query(`DELETE FROM class_videos WHERE id = $1 RETURNING *;`, [id]);
-      res.status(200).send(keysToCamel(deletedClassVideos));
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  });
+    const deletedClassVideos = await db.query(`DELETE FROM class_videos WHERE id = $1 RETURNING *;`, [id]);
+    res.status(200).send(keysToCamel(deletedClassVideos));
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
   
 
 
