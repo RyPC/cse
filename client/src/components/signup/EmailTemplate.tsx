26d67ff97c359a844a655f335d5eeaa2a8159068
Priyansh Shah <Priyanshpokemon@gmail.com>
2025-01-16 13:43:12 -0800

    adding the correct react-html package

---
+++ client/src/components/signup/EmailTemplate.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EmailTemplate({firstName, lastName, email, role}: {firstName: string, lastName: string, email: string, role: string}) {
+  return (
+    <Email title="Onboarding approval">
+
+    </Email>
+  )
+}
+
+export default EmailTemplate
