e17b2f256ea505ae6c64492f4121360eb6b33928
elliott <48373238+Satchel05@users.noreply.github.com>
2025-01-17 14:13:36 -0800

    Fix table creation syntax errors. Fix nonexistent reference columns

--- server/db/schema/reviews.sql
+++ server/db/schema/reviews.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS public.reviews(
     review VARCHAR(256),
 
     CONSTRAINT review_pkey PRIMARY KEY(class_id, student_id),
-    CONSTRAINT fk_class FOREIGN KEY(class_id) REFERENCES classes(class_id) ON DELETE CASCADE
-    CONSTRAINT fk_student FOREIGN KEY(student_id) REFERENCES students(student_id) ON DELETE CASCADE
+    CONSTRAINT fk_class FOREIGN KEY(class_id) REFERENCES classes(id) ON DELETE CASCADE,
+    CONSTRAINT fk_student FOREIGN KEY(student_id) REFERENCES students(id) ON DELETE CASCADE
 );
