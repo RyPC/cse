e9fef4f5a85450f50888f6ba7124d8b74dbee87b
Hydrohaven <hydrohaven2004@gmail.com>
2025-01-20 03:51:41 -0800

    Created ClassCard component

--- client/src/components/playground/Playground.jsx
+++ client/src/components/playground/Playground.jsx
@@ -1,9 +1,58 @@
-import { Box, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Box, Flex } from "@chakra-ui/react";
+
+import { useBackendContext } from "../../contexts/hooks/useBackendContext";
+import { ClassCard } from "../shared/ClassCard";
+
 
 export const Playground = () => {
-  return (
-    <Box>
-      <Text>This is page will be used to test any modal or component that does not have a specific place for it yet!</Text>
-    </Box>
-  );
+    const { backend } = useBackendContext();
+    
+    const [classes, setClasses] = useState([]);
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            // Fetch and Store Classes Information
+            try {
+                const response = await backend.get("/classes");
+                setClasses(response.data);
+            } catch (error) {
+                console.error("Error fetching classes:", error);
+            }
+            
+            // Fetch and Store Events Information
+            try {
+                const response = await backend.get("/events");
+                setEvents(response.data);
+            } catch (error) {
+                console.error("Error fetching events:", error);
+            }
+        };
+    
+        fetchData();
+    }, [backend]);
+
+    return (
+        <Box>
+            <Flex align="center" justify="center" gap={5} wrap="wrap">
+                {classes.map((classItem, index) => (
+                    <ClassCard 
+                        key={index}
+                        title={classItem.title}
+                        description={classItem.description}
+                        location={classItem.location}
+                        capacity={classItem.capacity}
+                        level={classItem.level}
+                        costume={classItem.costume}
+                    />
+                ))}
+
+                {events.map((eventItem, index) => (
+                    // Your event card component and its props!
+                    <></> // here to avoid errors
+                ))}
+            </Flex>
+        </Box>
+    );
 };
