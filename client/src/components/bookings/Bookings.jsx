64b6a59fece9718282cf8dc849e8528a8cf74761
Ryan Su <ryan.parkcity@gmail.com>
2025-01-21 13:55:50 -0800

    Added sample card and canceling modals

--- client/src/components/bookings/Bookings.jsx
+++ client/src/components/bookings/Bookings.jsx
@@ -6,6 +6,21 @@ import { Link } from "react-router-dom";
 
 export const Bookings = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const [currentModal, setCurrentModal] = useState("view");
+
+  const onCancel = () => {
+    setCurrentModal("cancel");
+  };
+  const onCloseModal = () => {
+    setCurrentModal("view");
+    onClose();
+  };
+  const onGoBack = () => {
+    setCurrentModal("view");
+  };
+  const onConfirm = () => {
+    setCurrentModal("confirmation");
+  };
 
   const handleClickEvents = () => {
     console.log("Booked events button has been pressed!");
@@ -17,6 +32,77 @@ export const Bookings = () => {
     console.log("Booked history button has been pressed!");
   };
 
+  const selectModal = () => {
+    if (currentModal === "view") {
+      return (
+        <Modal isOpen={isOpen} onClose={onCloseModal}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia cursus tortor et tempor. Mauris vulputate mattis feugiat. Fusce dignissim quis diam sit amet euismod. Vestibulum pulvinar interdum nisl. Aenean vel porta sem, id efficitur justo. Vestibulum vitae eros volutpat, tincidunt est interdum, hendrerit sem. Vestibulum porttitor orci a leo vulputate, vitae suscipit lacus tristique.
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme='red' mr={3} onClick={onCancel}>
+                Cancel
+              </Button>
+              <Button colorScheme='blue' mr={3} onClick={onCloseModal}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      );
+    }
+    else if (currentModal === "cancel") {
+      return (
+        <Modal isOpen={isOpen} onClose={onCloseModal}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+            Are you sure?
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme='red' mr={3} onClick={onConfirm}>
+                Confirm
+              </Button>
+              <Button colorScheme='blue' mr={3} onClick={onGoBack}>
+                Go back
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      );
+    }
+    else if (currentModal === "confirmation") {
+      return (
+        <Modal isOpen={isOpen} onClose={onCloseModal}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+            Changes made to class ____...
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia cursus tortor et tempor. Mauris vulputate mattis feugiat. Fusce dignissim quis diam sit amet euismod. Vestibulum pulvinar interdum nisl. Aenean vel porta sem, id efficitur justo. Vestibulum vitae eros volutpat, tincidunt est interdum, hendrerit sem. Vestibulum porttitor orci a leo vulputate, vitae suscipit lacus tristique.
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onCloseModal}>
+                Go Home
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      );
+    }
+  };
+
   return (
     <Box>
       <VStack
@@ -25,29 +111,21 @@ export const Bookings = () => {
       >
         <Heading>Bookings</Heading>
         <div>
-          <Button onClick={onOpen}>Events</Button>
+          <Button onClick={handleClickEvents}>Events</Button>
           <Button onClick={handleClickClasses}>Classes</Button>
           <Button onClick={handleClickHistory}>History</Button>
-          
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>Title</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                description
-              </ModalBody>
-    
-              <ModalFooter>
-                <Button colorScheme='blue' mr={3} onClick={onClose}>
-                  Cancel
-                </Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-
         </div>
+
+      <Box
+        p="50"
+        borderWidth="1px"
+        borderColor="black"
+        onClick={onOpen}
+      >
+        Class Sample
+      </Box>
       </VStack>
+      {selectModal()}
     </Box>
   );
 };
