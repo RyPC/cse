ae47c521d16ba4c69d9e924e52deda32702b7f5a
RaymondYan38 <raymondyan17@gmail.com>
2025-02-04 01:42:02 -0800

    introduce new components for creating classes

---
+++ client/src/components/forms/createClasses.jsx
@@ -0,0 +1,161 @@
+import { useState } from "react";
+
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  NumberInput,
+  NumberInputField,
+  Select,
+  Stack,
+  Text,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+import axios from "axios";
+
+import SaveClassAsDraftModal from "./modals/saveClassAsDraft";
+
+export const CreateClassForm = () => {
+  const sendAjax = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/classes/", {
+        location,
+        date,
+        description,
+        level,
+        capacity,
+        costume: performances,
+        isDraft: false,
+        title,
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+
+    setIsSubmitted(true);
+  };
+
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [capacity, setCapacity] = useState("");
+  const [level, setLevel] = useState("Beginner");
+  const [performances, setPerformances] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  return (
+    <Container>
+      <Text
+        fontSize="2xl"
+        textAlign="center"
+        mb={4}
+      >
+        New Class
+      </Text>
+      {!isSubmitted ? (
+        <form onSubmit={sendAjax}>
+          <FormControl>
+            <FormLabel>Class Title</FormLabel>
+            <Input
+              type="text"
+              required
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Location</FormLabel>
+            <Input
+              type="text"
+              required
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Date</FormLabel>
+            <Input
+              type="date"
+              required
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Description</FormLabel>
+            <Textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Capacity</FormLabel>
+            <NumberInput min={0}>
+              <NumberInputField
+                required
+                value={capacity}
+                onChange={(e) => setCapacity(e.target.value)}
+              />
+            </NumberInput>
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Level</FormLabel>
+            <Select
+              required
+              value={level}
+              onChange={(e) => setLevel(e.target.value)}
+            >
+              <option value="beginner">Beginner</option>
+              <option value="intermediate">Intermediate</option>
+              <option value="advanced">Advanced</option>
+            </Select>
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Performances</FormLabel>
+            <Input
+              type="text"
+              required
+              value={performances}
+              onChange={(e) => setPerformances(e.target.value)}
+            />
+          </FormControl>
+
+          <Stack
+            direction="row"
+            justifyContent="center"
+            mt={4}
+          >
+            <Button onClick={onOpen}>Save as Draft</Button>
+            <Button
+              colorScheme="blue"
+              type="submit"
+            >
+              Publish
+            </Button>
+          </Stack>
+        </form>
+      ) : (
+        <div>Form submitted</div>
+      )}
+
+      <SaveClassAsDraftModal
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+    </Container>
+  );
+};
+
+export default CreateClassForm;
--- client/src/components/playground/Playground.jsx
+++ client/src/components/playground/Playground.jsx
@@ -1,194 +1,64 @@
+import { useState } from "react";
+
 import {
+  Button,
+  Container,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
+  Heading,
   Input,
-  Stack,
-  Container,
-  Textarea,
-  Select,
-  Button,
   Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
   ModalBody,
   ModalCloseButton,
-  useDisclosure,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   NumberInput,
   NumberInputField,
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
-} from '@chakra-ui/react'
+  Select,
+  Stack,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
 
-import { useState } from 'react';
+import axios from "axios";
 
-import axios from 'axios';
-
-
+import CreateClassForm from "../forms/createClasses";
 
 export const Playground = () => {
-
-  const sendAjax = e => {
-
-    axios.post('http://localhost:3001/classes/', {
-      location: location,
-      date: date,
-      description: description,
-      level: level,
-      capacity: capacity,
-      costume: performances, // Need to confirm the equivalence between the two fields here
-      isDraft: false,
-      title: title
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  const sendAjax = (e) => {
+    axios
+      .post("http://localhost:3001/classes/", {
+        location: location,
+        date: date,
+        description: description,
+        level: level,
+        capacity: capacity,
+        costume: performances, // Need to confirm the equivalence between the two fields here
+        isDraft: false,
+        title: title,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
     setIsSubmitted(true);
-  
-  }
+  };
 
-  const [title, setTitle] = useState('');
-  const [location, setLocation] = useState('');
-  const [date, setDate] = useState('');
-  const [description, setDescription] = useState('');
-  const [capacity, setCapacity] = useState('');
-  const [level, setLevel] = useState('beginner');
-  const [performances, setPerformances] = useState('');
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [capacity, setCapacity] = useState("");
+  const [level, setLevel] = useState("Beginner");
+  const [performances, setPerformances] = useState("");
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  return (
-    
-    <Container>
-    {!isSubmitted ? (
-      <form onSubmit={sendAjax}>
-
-      <FormControl>
-      <FormLabel>Title</FormLabel>
-              <Input 
-                type='text' 
-                required 
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-      </FormControl>
-
-        
-        <Stack direction="horizontal">
-
-          <FormControl>
-              <FormLabel>Location</FormLabel>
-              <Input 
-                type='text' 
-                required 
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-              />
-          </FormControl>
-
-          <FormControl>
-              <FormLabel>Date</FormLabel>
-              <Input 
-                type='date' 
-                required 
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-              />
-          </FormControl>
-
-        </Stack>
-
-        <FormControl>
-          <FormLabel>Description</FormLabel>
-          <Textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          ></Textarea>
-        </FormControl>
-
-        <Stack direction="horizontal">
-          <FormControl>
-            <FormLabel>Capacity</FormLabel>
-            <NumberInput defaultValue={15} min={10} max={20}>
-              <NumberInputField 
-               required 
-               value={capacity}
-               onChange={(e) => setCapacity(e.target.value)}
-              />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel>Level</FormLabel>
-            <Select 
-              required
-              value={level}
-              onChange={(e) => setLevel(e.target.value)}
-            >
-              <option value="beginner">Beginner</option>
-              <option value="intermediate">Intermediate</option>
-              <option value="advanced">Advanced</option>
-            </Select>
-          </FormControl>
-        </Stack>
-
-        <FormControl>
-          <FormLabel>Performances</FormLabel>
-          <Input type='text' 
-            required 
-            value={performances}
-            onChange={(e) => setPerformances(e.target.value)}
-          />
-        </FormControl>
-
-        <br />
-
-        <>
-
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>Modal Title</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                fjasfdsafdsa
-              </ModalBody>
-
-              <ModalFooter>
-                <Button colorScheme='blue' mr={3} onClick={onClose}>
-                  Close
-                </Button>
-                <Button variant='blue'>Save as Draft</Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-        </>
-
-        <Stack direction="horizontal" float={"right"}>
-            <Button onClick={onOpen}>Save as Draft</Button>
-            <Button colorScheme='blue' type="submit">Publish</Button>
-        </Stack>
-      </form>
-
-    ) : (
-      <div>Form submitted</div>
-    )}
-    
-    </Container>
-
-  )
-
-}
+  return <CreateClassForm />;
+};
