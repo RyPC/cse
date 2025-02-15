7c8cd1988b8d6f84708b4daf6044509c7d20082e
Boyuan Liu <boyual7@uci.edu>
2025-02-14 21:01:47 -0800

    created a controller modal with select media and select tag modal

--- client/src/components/playground/Playground.jsx
+++ client/src/components/playground/Playground.jsx
@@ -1,64 +1,154 @@
 import { useState } from "react";
 
-import {
-  Button,
-  Container,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  NumberInput,
-  NumberInputField,
-  Select,
-  Stack,
-  Textarea,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { ConfirmationModal } from "../bookings/ConfirmationModal";
+import { CancelModal } from "../bookings/CancelModal";
 
-import axios from "axios";
+import { Button, Box, useDisclosure, Heading, VStack, Modal, ModalOverlay, ModalHeader, ModalContent, ModalCloseButton, ModalBody, ModalFooter, Input} from "@chakra-ui/react";
 
-import CreateClassForm from "../forms/createClasses";
 
-export const Playground = () => {
-  const sendAjax = (e) => {
-    axios
-      .post("http://localhost:3001/classes/", {
-        location: location,
-        date: date,
-        description: description,
-        level: level,
-        capacity: capacity,
-        costume: performances, // Need to confirm the equivalence between the two fields here
-        isDraft: false,
-        title: title,
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-
-    setIsSubmitted(true);
+const SelectMediaModal = ({ isOpen, onClose, setCurrentModal }) => {
+  const onPhoto = () => {
+    setCurrentModal("upload-photo");
   };
 
-  const [title, setTitle] = useState("");
-  const [location, setLocation] = useState("");
-  const [date, setDate] = useState("");
-  const [description, setDescription] = useState("");
-  const [capacity, setCapacity] = useState("");
-  const [level, setLevel] = useState("Beginner");
-  const [performances, setPerformances] = useState("");
+  const onLink = () => {
+    setCurrentModal("upload-link");
+  }
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Select Media</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Button onClick={onPhoto}>
+            Photo
+          </Button>
+          <Button onClick={onLink}>
+            Link
+          </Button>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
 
-  return <CreateClassForm />;
+const UploadLinkModal = ({ isOpen, onClose, setCurrentModal }) => {
+
+  const onGoBack = () => {
+    setCurrentModal("select-media")
+  }
+
+  const onConfirm = () => {
+    setCurrentModal("select-tag")
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Upload Link</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Input />
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme='red' mr={3} onClick={onConfirm}>
+            Next
+          </Button>
+          <Button colorScheme='blue' mr={3} onClick={onGoBack}>
+            Go back
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+const SelectTagModal = ({ isOpen, onClose, setCurrentModal }) => {
+
+  const onGoBack = () => {
+    setCurrentModal("select-media")
+  }
+
+  const onConfirm = () => {
+    setCurrentModal("card")
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Select Tag Modal</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Button>
+            Ballet
+          </Button>
+          <Button>
+            Classical
+          </Button>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme='red' mr={3} onClick={onConfirm}>
+            Confirm
+          </Button>
+          <Button colorScheme='blue' mr={3} onClick={onGoBack}>
+            Go back
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export const Playground = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [currentModal, setCurrentModal] = useState("select-media");
+  const [ tags, setTags ] = useState([])
+
+  const onCloseModal = () => {
+    setCurrentModal("select-media");
+    onClose();
+  };
+
+
+  const addTag = (value) => {
+    setTags(tags.concat([value]))
+  }
+
+
+  const removeTag = (value) => {
+    setTags(tags.filter(item => item !== value))
+  }
+
+
+  return (
+    <Box>
+      <VStack
+        spacing={8}
+        sx={{ maxWidth: "100%", marginX: "auto" }}
+      >
+        <Heading>Resource Flows for Teacher</Heading>
+
+
+      <Button onClick={onOpen}>
+        Initiate
+      </Button>
+      </VStack>
+      {
+        currentModal === "select-media" ?
+          <SelectMediaModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal} /> :
+        (currentModal === "upload-link" ?
+          <UploadLinkModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal}/> :
+        (currentModal === "select-tag" ?
+          <SelectTagModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal} /> :
+          <CancelModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal} />
+        )
+        )
+      }
+    </Box>
+
+  );
 };
