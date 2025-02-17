e1dae8b5c2f67b112ff590458dc9a0de2e67bbca
Boyuan Liu <boyual7@uci.edu>
2025-02-16 17:17:15 -0800

    finished most of the flow, missing submit to backend, s3 url generation, and upload file

--- client/src/components/playground/Playground.jsx
+++ client/src/components/playground/Playground.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-import { ConfirmationModal } from "../bookings/ConfirmationModal";
 import { CancelModal } from "../bookings/CancelModal";
 
-import { Button, Box, useDisclosure, Heading, VStack, Modal, ModalOverlay, ModalHeader, ModalContent, ModalCloseButton, ModalBody, ModalFooter, Input} from "@chakra-ui/react";
+import { Button, Box, useDisclosure, Heading, VStack, Modal, ModalOverlay, ModalHeader, ModalContent, ModalCloseButton, ModalBody, ModalFooter, Input, FormControl, Card, CardBody, CardFooter, Image, Divider, Tag} from "@chakra-ui/react";
 
+import { useBackendContext } from "../../contexts/hooks/useBackendContext";
 
 const SelectMediaModal = ({ isOpen, onClose, setCurrentModal }) => {
   const onPhoto = () => {
@@ -34,14 +34,23 @@ const SelectMediaModal = ({ isOpen, onClose, setCurrentModal }) => {
   );
 }
 
-const UploadLinkModal = ({ isOpen, onClose, setCurrentModal }) => {
+const UploadLinkModal = ({ isOpen, onClose, setCurrentModal, link, setLink }) => {
+
+  const isValidURL = string => {
+    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (res !== null)
+  };
 
   const onGoBack = () => {
     setCurrentModal("select-media")
   }
 
   const onConfirm = () => {
-    setCurrentModal("select-tag")
+    if (link.length == 0 || !isValidURL(link)) {
+      alert("Please type a valid link")
+    } else {
+      setCurrentModal("select-tag")
+    }
   }
 
   return (
@@ -51,7 +60,12 @@ const UploadLinkModal = ({ isOpen, onClose, setCurrentModal }) => {
         <ModalHeader>Upload Link</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Input />
+          <form>
+            <FormControl>
+              <Input value={link}
+              onChange={(e) => setLink(e.target.value)}/>
+            </FormControl>
+          </form>
         </ModalBody>
         <ModalFooter>
           <Button colorScheme='red' mr={3} onClick={onConfirm}>
@@ -66,14 +80,22 @@ const UploadLinkModal = ({ isOpen, onClose, setCurrentModal }) => {
   );
 }
 
-const SelectTagModal = ({ isOpen, onClose, setCurrentModal }) => {
+const SelectTagModal = ({ isOpen, onClose, setCurrentModal, tags, setTags }) => {
 
   const onGoBack = () => {
     setCurrentModal("select-media")
   }
 
   const onConfirm = () => {
-    setCurrentModal("card")
+    setCurrentModal("title")
+  }
+
+  const addTag = (value) => {
+    setTags(tags.concat([value]))
+  }
+
+  const removeTag = (value) => {
+    setTags(tags.filter(item => item !== value))
   }
 
   return (
@@ -83,10 +105,28 @@ const SelectTagModal = ({ isOpen, onClose, setCurrentModal }) => {
         <ModalHeader>Select Tag Modal</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Button>
+          <Button 
+          colorScheme={tags.includes("ballet") ? "teal" : "blue"}
+          onClick={() => {
+            if (tags.includes("ballet")) {
+              removeTag("ballet")
+            } else {
+              addTag("ballet")
+            }
+          }}
+          >
             Ballet
           </Button>
-          <Button>
+          <Button 
+          colorScheme={tags.includes("classical") ? "teal" : "blue"}
+          onClick={() => {
+            if (tags.includes("classical")) {
+              removeTag("classical")
+            } else {
+              addTag("classical")
+            }}
+          }
+          >
             Classical
           </Button>
         </ModalBody>
@@ -103,10 +143,98 @@ const SelectTagModal = ({ isOpen, onClose, setCurrentModal }) => {
   );
 }
 
+const TitleModal = ({ isOpen, onClose, setCurrentModal, title, setTitle }) => {
+  const onGoBack = () => {
+    setCurrentModal("select-tag")
+  }
+
+  const onConfirm = () => {
+    setCurrentModal("card")
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <form>
+            <FormControl>
+              <Input value={title}
+              onChange={(e) => {
+                if (e.target.value.length == 0) {
+                  alert("Title cannot be empty")
+                } else {
+                  setTitle(e.target.value)
+                }
+              }}/>
+            </FormControl>
+          </form>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme='red' mr={3} onClick={onConfirm}>
+            Next
+          </Button>
+          <Button colorScheme='blue' mr={3} onClick={onGoBack}>
+            Go back
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+
+const CardModal = ({ isOpen, onClose, setCurrentModal, title, tags, link, ajax }) => {
+  const onGoBack = () => {
+    setCurrentModal("title")
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Is everything correct?</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Card maxW='sm'>
+            <CardBody>
+              <Image
+                src={link}
+                alt='Green double couch with wooden legs'
+                borderRadius='lg'
+              />
+              <Heading>{title}</Heading>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              {tags.map((tag, index) => (
+                <Tag key={index}>{tag}</Tag>
+              ))}
+            </CardFooter>
+          </Card>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme='red' mr={3} onClick={ajax}>
+            Confirm
+          </Button>
+          <Button colorScheme='blue' mr={3} onClick={onGoBack}>
+            Go back
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+
 export const Playground = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [currentModal, setCurrentModal] = useState("select-media");
-  const [ tags, setTags ] = useState([])
+  const [ tags, setTags ] = useState([]);
+  const [ link, setLink ] = useState('');
+  const [ title, setTitle ] = useState('')
 
   const onCloseModal = () => {
     setCurrentModal("select-media");
@@ -114,15 +242,20 @@ export const Playground = () => {
   };
 
 
-  const addTag = (value) => {
-    setTags(tags.concat([value]))
+  const ajax = async () => {
+    const { backend } = useBackendContext();
+    await backend
+      .post("/classes-videos", {
+        title: title,
+        s3Url: "https://aws.com", // TODO: Need to obtain the actual S3 Url
+        description : "",
+        mediaUrl: link,
+        classId: 1 // TODO: Need to find the actual class id
+        // TODO: what to do with tags? 
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   }
-
-
-  const removeTag = (value) => {
-    setTags(tags.filter(item => item !== value))
-  }
-
 
   return (
     <Box>
@@ -141,10 +274,16 @@ export const Playground = () => {
         currentModal === "select-media" ?
           <SelectMediaModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal} /> :
         (currentModal === "upload-link" ?
-          <UploadLinkModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal}/> :
+          <UploadLinkModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal} link={link} setLink={setLink}/> :
         (currentModal === "select-tag" ?
-          <SelectTagModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal} /> :
+          <SelectTagModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal} tags={tags} setTags={setTags}/> :
+        (currentModal === "title" ? 
+          <TitleModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal} title={title} setTitle={setTitle}/> :
+        (currentModal === "card" ?
+          (  <CardModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal} title={title} tags={tags} link={link} ajax={ajax}/>) :
           <CancelModal isOpen={isOpen} onClose={onCloseModal} setCurrentModal={setCurrentModal} />
+        )
+        )
         )
         )
       }
