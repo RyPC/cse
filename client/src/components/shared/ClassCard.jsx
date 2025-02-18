b3f2233925f4c9df386090c66ee2cb289105fffa
RaymondYan38 <raymondyan17@gmail.com>
2025-02-17 20:31:45 -0800

    update cards to match mid-fi

--- client/src/components/shared/ClassCard.jsx
+++ client/src/components/shared/ClassCard.jsx
@@ -1,23 +1,82 @@
-import { Card, CardBody, CardHeader, CardFooter, Heading, VStack, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
-export const ClassCard = ({title, description, location, capacity, level, costume}) => {
-    return (
-        <Card w={{base: "80%", md: "20em"}}>
-            <CardHeader>
-                <Heading size="lg">{title}</Heading>
-            </CardHeader>
+import { FaClock, FaMapMarkerAlt, FaUser } from "react-icons/fa";
 
-            <CardBody>
-                <VStack>
-                    <Text>{description}</Text>
-                    <Text>{level} - {location}</Text>
-                    <Text>Costume: {costume}</Text>
-                </VStack>
-            </CardBody>
+import { formatDate, formatTime } from "../../utils/formatDateTime";
 
-            <CardFooter justifyContent="right">
-                <Text>0/{capacity} spots left</Text>
-            </CardFooter>
-        </Card>
-    );
+export const ClassCard = ({
+  title,
+  description,
+  location,
+  capacity,
+  level,
+  costume,
+  date,
+  startTime,
+  endTime,
+  attendeeCount = 0, // Default to 0 if not provided
+}) => {
+  const formattedDate = date ? formatDate(date) : null;
+  const formattedStartTime = startTime ? formatTime(startTime) : null;
+  const formattedEndTime = endTime ? formatTime(endTime) : null;
+  return (
+    <Card
+      w={{ base: "90%", md: "30em" }}
+      bg="gray.200"
+    >
+      <CardHeader pb={0}>
+        <Heading
+          size="md"
+          fontWeight="bold"
+        >
+          {title}
+        </Heading>
+      </CardHeader>
+      <CardBody>
+        <VStack
+          align="stretch"
+          spacing={2}
+        >
+          <HStack>
+            <FaClock size={14} />
+            <Text fontSize="sm">
+              {formattedDate} @ {formattedStartTime} - {formattedEndTime}
+            </Text>
+          </HStack>
+
+          <HStack>
+            <FaMapMarkerAlt size={14} />
+            <Text fontSize="sm">{location}</Text>
+          </HStack>
+
+          <HStack>
+            <FaUser size={14} />
+            <Text fontSize="sm">
+              {attendeeCount} {attendeeCount === 1 ? "person" : "people"} RSVP'd
+            </Text>
+          </HStack>
+          <Button
+            alignSelf="flex-end"
+            variant="solid"
+            size="sm"
+            bg="gray.500"
+            color="black"
+            _hover={{ bg: "gray.700" }}
+            mt={2}
+          >
+            View Details &gt;
+          </Button>
+        </VStack>
+      </CardBody>
+    </Card>
+  );
 };
