272b069910a9d7b51f304da019989978a4651bb8
Ethan Ho <hoea2@uci.edu>
2025-02-19 21:53:12 -0800

    added email sending on new teacher sign up

--- client/src/components/teacher-signup/TeacherSignup.tsx
+++ client/src/components/teacher-signup/TeacherSignup.tsx
@@ -24,6 +24,8 @@ import { z } from "zod";
 import { useAuthContext } from "../../contexts/hooks/useAuthContext";
 import { useBackendContext } from "../../contexts/hooks/useBackendContext";
 import { authenticateGoogleUser } from "../../utils/auth/providers";
+import { EmailTemplate } from "../signup/EmailTemplate";
+import ReactDOMServer from "react-dom/server"
 
 const signupSchema = z.object({
   firstName: z.string().min(1, "Field Cannot Be Empty"),
@@ -61,6 +63,11 @@ export const TeacherSignup = () => {
       });
 
       if (user) {
+        const templateEmail = EmailTemplate( {"firstName": data.firstName, "lastName":data.lastName, "email": data.email, "role": "teacher"});
+        backend.post("/nodemailer/send", { 
+          "to": import.meta.env.VITE_ADMIN_EMAIL, 
+          "html": ReactDOMServer.renderToString(templateEmail)
+        });
         navigate("/teacher-signup/request");
       }
     } catch (err) {
