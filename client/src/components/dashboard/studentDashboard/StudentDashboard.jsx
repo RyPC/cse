60113a8fdaf4ecebf6aa339904df3b8fd61f112a
Ryan Su <ryan.parkcity@gmail.com>
2025-04-09 16:39:14 -0700

    Updated admin dashboard students tab to match hifi

--- client/src/components/dashboard/studentDashboard/StudentDashboard.jsx
+++ client/src/components/dashboard/studentDashboard/StudentDashboard.jsx
@@ -1,76 +1,167 @@
-import { Image, Link, Table, TableContainer, Tbody, Td, Th, Thead, Stack, Flex, Tr, Heading, useDisclosure } from "@chakra-ui/react";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Stack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
+
+import { FiTrash2 } from "react-icons/fi";
+import { SlArrowLeft } from "react-icons/sl";
 
 import { useBackendContext } from "../../../contexts/hooks/useBackendContext";
-import { NotificationPanel } from "../NotificationPanel"; 
+import { NotificationPanel } from "../NotificationPanel";
 
 export const StudentDashboard = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const { notifRef } = useRef();
-    const { backend } = useBackendContext();
-    const [ students, setStudents ] = useState([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { notifRef } = useRef();
+  const { backend } = useBackendContext();
+  const [students, setStudents] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await backend.get("/students");
-                setStudents(response.data);
-            } catch (error) {
-                console.error("Error fetching students:", error);
-            }
-        };
-    
-        fetchData();
-    }, [backend]);
-    
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await backend.get("/students");
+        setStudents(response.data);
+      } catch (error) {
+        console.error("Error fetching students:", error);
+      }
+    };
 
-    return (
-        <Stack mx="10%" my={5}>
-            <Flex direction="row" justify="flex-end">
-                <Image 
-                    cursor="pointer"
-                    onClick={onOpen}
-                    ref={notifRef}
-                    src="../bell.png"
-                />
-            </Flex>
-            <NotificationPanel isOpen={isOpen} onClose={onClose} />
+    fetchData();
+  }, [backend]);
 
-            <Heading my={5}>Students</Heading>
-            <TableContainer 
-                sx={{
-                    overflowX: "auto",
-                }}
-            >
-                <Table variant="simple">
-                    <Thead>
-                        <Tr>
-                        <Th>Id</Th>
-                        <Th>Name</Th>
-                        <Th>Email</Th>
-                        <Th>Level</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {students
-                        ? students.map((stud, i) => (
-                            <Tr key={i}>
-                                <Td>{stud.id}</Td>
-                                <Td>
-                                    <Link
-                                        href={`/dashboard/students/${stud.id}`}
-                                    >   
-                                        {stud.firstName} {stud.lastName}
-                                    </Link>
-                                </Td>
-                                <Td>{stud.email}</Td>
-                                <Td>{stud.level}</Td>
-                            </Tr>
-                            ))
-                        : null}
-                    </Tbody>
-                </Table>
-            </TableContainer>
-        </Stack>
-    );
+  return (
+    <VStack>
+      <Flex
+        w={"100%"}
+        justify={"space-between"}
+      >
+        <Heading
+          as="h1"
+          size="lg"
+          mb={4}
+          alignSelf={"flex-start"}
+          alignContent="center"
+          fontWeight={700}
+          fontSize={36}
+        >
+          <Button
+            backgroundColor="transparent"
+            fontSize={25}
+            mb={1}
+            onClick={() => navigate("/dashboard")}
+          >
+            <SlArrowLeft />
+          </Button>
+          Students
+        </Heading>
+        <Image
+          alignSelf={"flex-end"}
+          cursor="pointer"
+          onClick={onOpen}
+          ref={notifRef}
+          src="../bell.png"
+        />
+        <NotificationPanel
+          isOpen={isOpen}
+          onClose={onClose}
+        />
+      </Flex>
+      <Box
+        w="100%"
+        pl={20}
+      >
+        SEARCH BAR HERE
+      </Box>
+      <TableContainer
+        w="100%"
+        sx={{
+          overflowX: "auto",
+        }}
+        pl={20}
+      >
+        <Table colorScheme="gray">
+          <Thead>
+            <Tr>
+              <Th
+                fontFamily="Inter"
+                fontWeight={700}
+                color="#4A5568"
+                letterSpacing="5%"
+                fontSize={18}
+                textTransform="none"
+              >
+                Student
+              </Th>
+              <Th
+                fontFamily="Inter"
+                fontWeight={700}
+                color="#4A5568"
+                letterSpacing="5%"
+                fontSize={18}
+                textTransform="none"
+              >
+                Email
+              </Th>
+              <Th
+                fontFamily="Inter"
+                fontWeight={700}
+                color="#4A5568"
+                letterSpacing="5%"
+                fontSize={18}
+                textTransform="none"
+              >
+                Classes
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {students
+              ? students.map((stud, index) => (
+                  <Tr
+                    key={stud.id}
+                    onClick={() => navigate(`/dashboard/students/${stud.id}`)}
+                    backgroundColor={index % 2 ? "white" : "gray.100"} // Striped row backgrounds
+                    _hover={{ bg: "gray.300", cursor: "pointer" }}
+                    color="gray.700"
+                  >
+                    <Td>
+                      {stud.firstName} {stud.lastName}
+                    </Td>
+                    <Td>{stud.email}</Td>
+                    <Td>{/** NUMBER OF CLASSES **/}</Td>
+                    <Td>
+                      <Button
+                        backgroundColor="transparent"
+                        onClick={(e) => {
+                          e.stopPropagation(); // prevents earlier onclick
+                        }}
+                        m={-8} // overrides bounds of row
+                        fontSize="28px"
+                      >
+                        <FiTrash2 />
+                      </Button>
+                    </Td>
+                  </Tr>
+                ))
+              : null}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </VStack>
+  );
 };
