828ff58bc8b24a254f4930ea5887ffb20c090d3c
Bowen Wang <bowensw@uci.edu>
2025-04-09 14:51:50 -0700

    finished user case 2 and 3

--- client/src/components/bookings/TeacherConfirmationModal.jsx
+++ client/src/components/bookings/TeacherConfirmationModal.jsx
@@ -9,13 +9,12 @@ export const TeacherConfirmationModal = ({ isOpen, onClose }) => {
       <ModalContent>
         <VStack>
           <Flex paddingTop={10} justifyContent="center" w="100%" position="relative">
-            <Icon as={BsCheck} boxSize="75vw" backgroundColor="#D9D9D9" borderRadius="full" color="#323232"></Icon>
+            <Icon as={BsCheck} boxSize="75vw" backgroundColor="#134A74" borderRadius="full" color="white"></Icon>
           </Flex>
           <Text fontSize="28px" fontWeight="bold">Class Deleted</Text>
         <ModalFooter>
           <Button backgroundColor="#D9D9D9" padding={7} onClick={onClose}>
-            Back to Booked Classes Page
-          </Button>
+          View Booked Events          </Button>
         </ModalFooter>
         </VStack>
       </ModalContent>
