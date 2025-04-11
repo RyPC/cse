2261bfe13046f4f0a5d422583a7d20570862cd69
Adarsh Venkateswaran <adarsv1@uci.edu>
2025-04-10 18:58:40 -0700

    styling mostly up to hifi, some things missing

--- client/src/components/bookings/ConfirmationModal.jsx
+++ client/src/components/bookings/ConfirmationModal.jsx
@@ -9,7 +9,6 @@ export const ConfirmationModal = ({ isOpen, onClose, card }) => {
         <ModalOverlay />
         <ModalContent>
             <ModalHeader py={3}>
-                
             </ModalHeader>
             <VStack>
                 <Box
