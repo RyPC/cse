91a4dea4eb73b8bad6377ac2b68c7f63082656cf
Joshua Micah Sullivan <joshee.sullivan@gmail.com>
2025-04-13 11:29:38 -0700

    Updated Video & Article cards to display s3 images

--- client/src/components/resources/NewsCard.jsx
+++ client/src/components/resources/NewsCard.jsx
@@ -1,17 +1,15 @@
-import { Card, CardBody, Text, Stack } from "@chakra-ui/react";
+import { Card, CardBody, Text, Stack, Link, Image } from "@chakra-ui/react";
 
 export const NewsCard = ({ id, S3Url, description, mediaUrl, tags }) => {
   return (
     <Card w={{base: "80%", md: "20em"}}>
       <CardBody>
         <Stack gap={2}>
-          {/* <Text>ID: {id}</Text> */}
-          {/* <Text>S3 URL: {S3Url}</Text> */}
           <Text fontSize="md">{description ?? "No description"}</Text>
-          <Text fontSize="sm">Posted by "Instructor Name" for "Class Name"</Text>
-          <Text>Media URL: {mediaUrl}</Text>
+          <Link href={mediaUrl} isExternal>
+            <Image src={S3Url} alt={`image for video article ${id}`} />
+          </Link>
           {tags?.length && <Text>Tags: {tags.join(', ')}</Text>}
-          {/* <Text>Tags: {tags?.length ? tags.join(', ') : 'No tags!'}</Text> */}
         </Stack>
       </CardBody>
     </Card>
--- client/src/components/resources/VideoCard.jsx
+++ client/src/components/resources/VideoCard.jsx
@@ -1,18 +1,18 @@
-import { Card, CardHeader, CardBody, CardFooter, Text, Heading, Stack } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, CardFooter, Text, Heading, Stack, Image, Link } from "@chakra-ui/react";
 
-export const VideoCard = ({ id, title, description, S3Url, classId, mediaUrl, tags }) => {
+export const VideoCard = ({ id, title, description, S3Url, classId, classTitle, mediaUrl, tags }) => {
+  // console.log(S3Url);
   return (
     <Card w={{base: "100%", md: "20em"}}>
       <CardBody>
         <Stack gap={2}>
           <Heading size="md">{title}</Heading>
-          {/* <Text>ID: {id}</Text> */}
-          {/* <Text>S3 URL: {S3Url}</Text> */}
           <Text fontSize="md">{description ?? "No description"}</Text>
-          <Text fontSize="sm">Posted by "Instructor Name" for "Class Name"</Text>
-          <Text>Media URL: {mediaUrl}</Text>
+          <Text fontSize="sm">Posted by "Instructor Name" for {classTitle}</Text> {/* Implement Instructor Name at later task! - josh :D */}
+          <Link href={mediaUrl} isExternal>
+            <Image src={S3Url} alt={`image for video ${title}`} />
+          </Link>
           {tags?.length && <Text>Tags: {tags.join(', ')}</Text>}
-          {/* <Text>Tags: {tags?.length ? tags.join(', ') : 'No tags!'}</Text> */}
         </Stack>
       </CardBody>
     </Card>
