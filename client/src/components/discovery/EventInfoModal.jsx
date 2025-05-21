cac20a4a2200cd31fad8a11c38875c8a8618c147
Boyuan Liu <boyual7@uci.edu>
2025-05-21 12:28:17 -0700

    made discovery/event UI match HI-FI

--- client/src/components/discovery/EventInfoModal.jsx
+++ client/src/components/discovery/EventInfoModal.jsx
@@ -18,6 +18,8 @@ import {
   ModalOverlay,
   Text,
   VStack,
+  Tag,
+  Divider
 } from "@chakra-ui/react";
 import { FaTimesCircle } from "react-icons/fa";
 import { FaCheckCircle } from "react-icons/fa";
@@ -73,6 +75,26 @@ function EventInfoModal({
   //   }
   // }, [backend, id, user?.data, isOpenProp]);
 
+  const [tags, setTags] = useState([]);
+  const [startTime, setStartTime] = useState("");
+
+  const getStartTime = async() => {
+    const data = await backend.get(`/events/${id}`)
+    setStartTime(data.data[0].startTime)
+  }
+
+  const getTags = async () => {
+    let tags_arr = [];
+    const tags = await backend.get(`/event-tags/tags/${id}`)
+    console.log("TAGS from GETTAGS event")
+    console.log(tags.data)
+    for (let i=0; i<tags.data.length; i++) {
+      if (!tags_arr.includes(tags.data[i].tag)) {
+        tags_arr.push(tags.data[i].tag)
+      }
+    }
+    setTags(tags_arr)
+  }
 
   const enrollInEvent = async () => {
     // Check if already checked into event
@@ -118,6 +140,11 @@ function EventInfoModal({
     handleClose();
   };
 
+  const parseDate = (dateString) => {
+    // https://stackoverflow.com/questions/11591854/format-date-to-mm-dd-yyyy-in-javascript
+    const date = new Date(dateString)
+    return ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear()
+  }
   
 
   useEffect(() => {
@@ -126,6 +153,8 @@ function EventInfoModal({
         .then((res) => res.json())
         .then((data) => setImageSrc(data.message));
     }
+    getTags()
+    getStartTime()
   }, [imageSrc, isOpenProp]);
 
   return (
@@ -145,16 +174,60 @@ function EventInfoModal({
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>
+            <List>
+              {tags.map((tag, index) => (
+                <Tag key={index}>
+                  {tag}
+                </Tag>
+              ))}
+            </List> 
             <Flex justifyContent="center">
               {title}
             </Flex>
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody>
+            <Text>Description: {description}</Text> <br />
+            <Divider orientation='horizontal' /> <br /> 
+            <Text color="#553C9A">Date: {parseDate(date)} {startTime}</Text> 
+            <Text>Location: {location}</Text>     
+            <Divider orientation='horizontal' /> <br /> 
             <VStack
               spacing={4}
               align="center"
             >
+
+              <Box
+                boxSize="sm"
+                height="15rem"
+                width={"100%"}
+                alignContent={"center"}
+                justifyContent={"center"}
+                display="flex"
+              >
+                <Image
+                  src={imageSrc}
+                  alt="Random Dog"
+                  width={"100%%"}
+                />
+              </Box>
+              
+              
+              <HStack
+                spacing={4}
+                width={"100%"}
+                justifyContent={"space-between"}
+              >
+                <Box>
+                  <Text fontWeight="bold">Capacity</Text>
+                  <Text>{capacity}</Text>
+                </Box>
+                <Box>
+                  <Text fontWeight="bold">Level</Text>
+                  <Text>{level}</Text>
+                </Box>
+              </HStack>
+              <Divider orientation='horizontal' /> 
               <HStack width="100%">
                 {!isCorequisiteSignUp && (
                   <Box bg = "#E8E7EF" borderRadius="md" width = "100%" p={4}>
@@ -182,59 +255,6 @@ function EventInfoModal({
                   </Box>
                 )}
               </HStack>
-              <Box
-                boxSize="sm"
-                height="15rem"
-                width={"100%"}
-                alignContent={"center"}
-                justifyContent={"center"}
-                display="flex"
-              >
-                <Image
-                  src={imageSrc}
-                  alt="Random Dog"
-                  width={"100%%"}
-                />
-              </Box>
-
-              <HStack
-                width={"100%"}
-                justifyContent={"space-between"}
-              >
-                <Box>
-                  <Text fontWeight="bold">Location:</Text>
-                  <Text>{location}</Text>
-                </Box>
-                <Box>
-                  <Text fontWeight="bold">Date</Text>
-                  <Text>{formatDate(date)}</Text>
-                </Box>
-              </HStack>
-              
-              <Box width="100%">
-                <Box>
-                    <Text fontWeight="bold">Time</Text>
-                    <Text>need to pass in time prop</Text>
-                </Box>
-                <Text fontWeight="bold">Description:</Text>
-                <Text>{description}</Text> 
-              </Box>
-
-              <HStack
-                spacing={4}
-                width={"100%"}
-                justifyContent={"space-between"}
-              >
-                <Box>
-                  <Text fontWeight="bold">Capacity</Text>
-                  <Text>{capacity}</Text>
-                </Box>
-                <Box>
-                  <Text fontWeight="bold">Level</Text>
-                  <Text>{level}</Text>
-                </Box>
-              </HStack>
-
               <HStack width={"100%"}>
                 <Box>
                   <Text fontWeight="bold">Classes</Text>
